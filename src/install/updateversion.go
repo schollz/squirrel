0a9e04f2d26d23e6ef7d6e9fb9f3928612019295
Zack Scholl <zack.scholl@gmail.com>
2019-05-04 06:59:12 -0700

    fix ui

--- src/install/updateversion.go
+++ src/install/updateversion.go
@@ -33,6 +33,11 @@ func run() (err error) {
 		fmt.Printf("updated README to version %s\n", strings.Split(versionNew, "-")[0])
 	}
 
+	err = replaceInFile("src/cli/cli.go", `Version = "`, `"`, versionNew+"-"+versionHashNew)
+	if err == nil {
+		fmt.Printf("updated cli.go to version %s\n", versionNew)
+}
+
 	err = replaceInFile("src/install/default.txt", `squirrel_version="`, `"`, strings.Split(versionNew, "-")[0][1:])
 	if err == nil {
 		fmt.Printf("updated default.txt to version %s\n", strings.Split(versionNew, "-")[0][1:])
