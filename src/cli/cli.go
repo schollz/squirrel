01d546e9606482bd7753d530be3e4bdd807e6f58
Zack Scholl <zack.scholl@gmail.com>
2019-05-10 08:12:00 -0600

    bump

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -22,7 +22,7 @@ func Run() (err error) {
 
 	app := cli.NewApp()
 	app.Name = "squirrel"
-	app.Version = "v1.0.3-7820adc"
+	app.Version = "v1.0.4-8726308"
 	app.Compiled = time.Now()
 	app.Usage = "download URLs directly into an SQLite database"
 	app.Flags = []cli.Flag{
